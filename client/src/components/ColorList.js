3b1159b915a92f4872f5f3cae1f909f7ae4e6ace
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-23 10:44:09 -0500

    WIP: created PUT request to edit colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,6 +22,16 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        const index = colors.findIndex(color => color.id === colorToEdit.id);
+        colors[index] = colorToEdit;
+        updateColors(colors);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
   };
 
   const deleteColor = color => {
